921314f20b176e9d540dbbc640e2f3fdf5a2eb34
Brandon Stine <brandon.stine99@gmail.com>
2023-07-14 00:46:30 -0400

    Update run.sh

--- Phase 1/run.sh
+++ Phase 1/run.sh
@@ -9,7 +9,7 @@
 #SBATCH --mem-per-cpu=16GB 
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
-#SBATCH --mail-user=<brandon.stine@knights.ucf.edu>
+#SBATCH --mail-user=<br696985@ucf.edu>
 #SBATCH --output=hoomd-%j.out
 
 # ==========================================================

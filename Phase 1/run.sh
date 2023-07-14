9561e94c2f45d5686dfcf5ccfb2ffa37b222fdc7
Brandon Stine <brandon.stine99@gmail.com>
2023-07-13 22:46:38 -0400

    Update run.sh

--- Phase 1/run.sh
+++ Phase 1/run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #SBATCH --nodes=1
-#SBATCH --time=40:00:00
+#SBATCH --time=100:00:00
 #SBATCH --job-name=hoomd
-#SBATCH --cores-per-socket=12
-#SBATCH --ntasks-per-node=12     # number of tasks per node
+#SBATCH --cores-per-socket=8
+#SBATCH --ntasks-per-node=4     # number of tasks per node
 #SBATCH --cpu-freq=HighM1[Performance]
 #SBATCH --shared
-#SBATCH --mem-per-cpu=5GB 
+#SBATCH --mem-per-cpu=16GB 
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=<brandon.stine@knights.ucf.edu>
